50519314d564791e074ccc9d4e2ed9da7b5d69b3
AdhiRajasaRafif02 <adhi.r.rafif@gmail.com>
2025-05-04 20:54:41 +0700

    Vite fix

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.REACT_APP_API_BASE_URL': JSON.stringify(process.env.REACT_APP_API_BASE_URL),
+    'process.env.REACT_APP_API_BASE_URL': JSON.stringify(
+      process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000' // Fallback to localhost
+    ),
   },
 });
